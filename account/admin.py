8f148c9050058de21a8cc95183ae66b70c1302a5
mchtbyrm <m.byrm45@icloud.com>
2023-08-04 11:33:18 +0300

    CustomUserAdmin added

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from account.models import CustomUser, Profile
+from django.contrib.auth.admin import UserAdmin
+
+from account.models import CustomUser, Profile, ProfilePicture
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', )}),
+    )
+
 
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Profile)
+admin.site.register(ProfilePicture)
